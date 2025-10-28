77b5a1d731f4e5be17f9f8b17a68b2023b7d98b7
mohamed <egyptgoschool@gmail.com>
2025-10-28 05:53:01 +0300

    feat: add file and folder components with SVG icons
    - Implemented FileComponent to display files and folders with icons.
    - Created SVG icons for File, Folder, Right Arrow, Bottom Arrow, and Close.
    - Defined a file tree structure in fileTree.ts for demonstration.
    - Added IFile interface to represent file and folder structure.
    - Introduced svgStyles for consistent SVG sizing across components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import FileComponent from './components/FileComponent'
+import { fileTree } from './data/fileTree'
 
-function App() {
-  return (
-    <>
-    <div className="text-3xl font-bold text-center underline">Hello world!</div>
-    </>
-  )
-}
+  function App() {
+    return (
+      <div>
+        {
+          fileTree.children && fileTree.children?.map((file, index) => (
+            <FileComponent isFolder={file.isFolder} filename={file.name} key={index} />
+          ))
+        }
+      </div>
+    )
+  }
 
-export default App
+  export default App
